32c8179ef5a4a236202fb582cb03ebd6f6495eee
Brock Janssen <b.alan.janssen@gmail.com>
2024-02-06 09:46:28 -0600

    added styling to lists

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,6 @@ function App() {
                 {!userLoggedIn
                     ? <PreviewPane/>
                     : <SelectionWidget />}
-                <DragList />
             </AppContext.Provider>
         </div>
     )
