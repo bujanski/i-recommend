8e48c29273c360ebee264bc5f5892428ae2e9c6d
Brock Janssen <b.alan.janssen@gmail.com>
2024-02-27 22:28:45 -0600

    selection widget modified to accept a category prop

--- client/src/components/SelectionWidget.js
+++ client/src/components/SelectionWidget.js
@@ -4,26 +4,30 @@ import DragList from "./DragList";
 import UserFavList from "./UserFavList";
 import SearchBar from "./SearchBar";
 
-function SelectionWidget() {
+function formatCategoryText(category) {
+    return category.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
+}
+
+function SelectionWidget({category}) {
     const { state, dispatch } = useContext(AppContext);
-    const { selectingCategory, addingItem } = state;
-    const lCaseCategory = selectingCategory.toLowerCase();
+    const { addingItem } = state;
+    const formattedCategory = formatCategoryText(category);
 
     const startAddItem = () => {
-        dispatch({ type: "startAddItem", payload: selectingCategory });
+        dispatch({ type: "startAddItem", payload: category });
     };
 
     return (
         <div className="selection-widget">
-            <h2>Your favorite movies</h2>
+            <h2>Your favorite {formattedCategory}</h2>
             <div className="search-tool">
                 <div className="add-item-button" onClick={startAddItem}>
-                    + Add {lCaseCategory} to your list
+                    + Add {formattedCategory} to your list
                 </div>
                 {addingItem ? <SearchBar /> : null}
             </div>
             <div>
-                <UserFavList id={1} category={selectingCategory} />
+                <UserFavList id={1} category={category} />
             </div>
             <div>
                 <button>Expand to see all your favorites</button>
