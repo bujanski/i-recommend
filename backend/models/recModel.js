ca2fa80fce7255f93cf7774821e4c9a2ee3f749a
fsaberex <69372284+fsaberex@users.noreply.github.com>
2024-02-28 19:59:46 -0500

    recommendations data set

--- backend/models/recModel.js
+++ backend/models/recModel.js
@@ -2,7 +2,13 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/connection");
 
 const Recommendation = sequelize.define('Recommendations', {
-
+    ranking: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    comment: {
+        type: DataTypes.TEXT,
+    }
 }, {
     timestamps: false
 });
